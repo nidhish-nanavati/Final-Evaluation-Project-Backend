ea6638e849a31ef5e69b3b6e89a72c28a121f6ec
Nidhish Nanavati <nanostudmaterial@gmail.com>
2024-12-24 21:34:20 +0530

    Create a backend API

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const userRoute = require("./routes/user");
+const cors = require('cors');
+
+dotenv.config();
+// app.get('/',(req,res)=>{
+//     // res.send("Hello World");
+//     res.sendFile(path.join(__dirname,"public","try.html")); 
+// });
+
+// Define a health API check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'UP',
+        timestamp: new Date().toISOString()
+    });
+});
+
+
+// Configure CORS middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use("/api/user",userRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`,);
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>{
+        console.log("MongoDB connected");
+    }).catch(error => {
+        console.log(error);
+    })
+})
---
+++ routes/user.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../schema/user.schema");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+router.post('/register',async (req,res)=>{
+    
+    const {username,email,password} = req.body;
+    const isUserExist = await User.findOne({email});
+    if(isUserExist){
+        return res.status(400).json({message:"User already exists"});
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
+    try{
+        const user = await User.create({
+            username : username,
+            email : email,
+            password: hashedPassword,
+        });
+        res.status(200).json({message:"User created"});
+    }
+    catch(err){
+        res.status(500).json({message: err});
+    }
+})
+
+router.post('/login',async (req,res) => {
+    const { email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).json({message :'Wrong username'});
+    }
+    const isPasswordCorrect = await bcrypt.compare(password,user.password)
+    if(!isPasswordCorrect){
+        return res.status(400).json({message :'Wrong credentials'});
+    }
+
+    const payload = {
+        id : user._id,
+    }
+
+    const token = jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn : "1h",
+    });
+    res.status(200).json({message : "User logged in succesfully" , token : token});
+
+});
+
+module.exports = router
