ad9652d871ffea97636ccc49c046ecb43544fa4c
Nidhish Nanavati <nanostudmaterial@gmail.com>
2024-12-24 21:51:37 +0530

    Added vercel configurations

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,20 @@ const User = require("../schema/user.schema");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const userSchema = require('../schema/user.schema');
 
 dotenv.config();
+
+
+router.get('/',async (req,res)=>{
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    }
+    catch(err){
+        res.status({message: err});
+    }
+})
 
 router.post('/register',async (req,res)=>{
     
